330bb6286f0aa6d8a3db850f4704abb2d74c934f
OTPOBEH <3akobah@gmail.com>
2018-01-24 21:32:43 +0200

    A switch statement has been added...

--- 5.Calculate Sequence with Queue/CalculateSequenceWithQueue.cs
+++ 5.Calculate Sequence with Queue/CalculateSequenceWithQueue.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class CalculateSequenceWithQueue
 {
     static void Main()
     {
+        var current = int.Parse(Console.ReadLine());
+        var counter = 1;
+        var sequence = new Queue<int>();
+        var currentTemp = 0;
 
+        sequence.Enqueue(current);
+        Console.Write(current + " ");
 
+        while (counter != 50)
+        {
+            switch (counter % 4)
+            {
+                case 1:
 
-
+                    Console.Write(sequence.Sum() + " ");
+                    break;
+                case 2:
+                    
+                    Console.Write(sequence.Sum() + " ");
+                    break;
+                case 3:
+;
+                    Console.Write(sequence.Sum() + " ");
+                    break;
+                case 0:
+ 
+                    break;
+            }
+            counter++;
+        }
     }
 }
