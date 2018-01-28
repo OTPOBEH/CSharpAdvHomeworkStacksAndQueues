690e6d006f66650aa18db64437ce7b88d7a459d7
OTPOBEH <3akobah@gmail.com>
2018-01-28 09:54:10 +0200

    Added Problem 6 "Truck Tour"solution...

---
+++ 6.TruckTour/TruckTour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class TruckTour
+{
+    static void Main()
+    {
+        var pumpsTotal = int.Parse(Console.ReadLine());
+        var fuelData   = new Queue<int>();
+        var startIndex = 0;
+
+        for (int i = 0; i < pumpsTotal; i++)
+        {
+            var input     = Console.ReadLine()?.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var fuel      = input[0];
+            var distance  = input[1];
+            var fuelSpent = fuel - distance;
+            fuelData.Enqueue(fuelSpent);
+        }
+
+        while (true)
+        {
+            var fuelInTank = 0;
+            for (int i = 0; fuelInTank >= 0; i++)
+            {
+                fuelInTank += fuelData.ElementAt(i);
+                if (i == pumpsTotal - 1)
+                {
+                    Console.WriteLine(startIndex);
+                    return;
+                }
+            }
+
+            var currentPump = fuelData.Dequeue();
+            fuelData.Enqueue(currentPump);
+            startIndex++;
+        }
+    }
+}
