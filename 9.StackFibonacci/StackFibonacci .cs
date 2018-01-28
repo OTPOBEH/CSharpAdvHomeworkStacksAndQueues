0f095fa9e10dbdee7d225e41b451b9327b84d9f3
OTPOBEH <3akobah@gmail.com>
2018-01-28 23:02:50 +0200

    Added Problem 9. Stack Fibonacci solution...

---
+++ 9.StackFibonacci/StackFibonacci .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StackFibonacci
+{
+    static void Main()
+    {
+        var requiredFubonacciNumber = int.Parse(Console.ReadLine());
+        var temp = new long[] { 1, 1 };
+
+        if (requiredFubonacciNumber <= 2)
+        {
+            Console.WriteLine(temp[requiredFubonacciNumber - 1]);
+            return;
+        }
+
+        var sequence = new Stack<long>(temp);
+
+        for (int i = 2; i < requiredFubonacciNumber; i++)
+        {
+            var second = sequence.Pop();
+            var first = sequence.Peek();
+
+            var third = first + second;
+            sequence.Push(second);
+            sequence.Push(third);
+        }
+        Console.WriteLine(sequence.Peek());
+    }
+}
