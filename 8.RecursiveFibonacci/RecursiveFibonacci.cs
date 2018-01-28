78b72d1182319ee37f2c4b9f23b63f7e1a92ca8d
OTPOBEH <3akobah@gmail.com>
2018-01-28 22:48:10 +0200

    Added 8.Recursive Fibonacci solution...

---
+++ 8.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class RecursiveFibonacci
+{
+    static void Main()
+    {
+        var requiredFubonacciNumber = int.Parse(Console.ReadLine());
+        var temp = new long[] { 1, 1 };
+
+        if (requiredFubonacciNumber <= 2)
+        {
+            Console.WriteLine(temp[requiredFubonacciNumber - 1]);
+            return;
+        }
+
+        var sequence = new Queue<long>(temp);
+        long third = 0;
+
+        for (int i = 2; i < requiredFubonacciNumber; i++)
+        {
+            var first = sequence.Dequeue();
+            var second = sequence.Peek();
+
+            third = first + second;
+
+            sequence.Enqueue(third);
+        }
+        Console.WriteLine(third);
+    }
+}
