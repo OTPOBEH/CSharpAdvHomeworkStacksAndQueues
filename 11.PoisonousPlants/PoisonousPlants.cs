c1c723514b9cdd27f9005eff21f9b74cfa1db97e
Stefan Petrov <3akobah@gmail.com>
2018-01-29 16:32:51 +0200

    11. Poisonous Plants solution updated...

---
+++ 11.PoisonousPlants/PoisonousPlants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PoisonousPlants
+{
+    static void Main()
+    {
+        var flowers = int.Parse(Console.ReadLine());
+        var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var flowerField1 = new Queue<int>(input);
+
+        Console.WriteLine();
+        var initialCount = 0;
+        var days = 0;
+
+        while (initialCount != flowerField1.Count)
+        {
+            initialCount = flowerField1.Count;
+            var previousFlower = flowerField1.Dequeue();
+            flowerField1.Enqueue(previousFlower);
+
+            for (int i = 0; i < initialCount; i++)
+            {
+                var currentFlower = flowerField1.Dequeue();
+                if (previousFlower < currentFlower)
+                {
+                    flowerField1.Enqueue(currentFlower);
+                }
+                previousFlower = currentFlower;
+            }
+
+            days++;
+        }
+    }
+}
