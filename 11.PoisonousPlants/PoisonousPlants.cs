7aa9d44f2a16939c82bd51ca55ff59b402442955
Stefan Petrov <3akobah@gmail.com>
2018-01-29 16:52:15 +0200

    Solution updated...

--- 11.PoisonousPlants/PoisonousPlants.cs
+++ 11.PoisonousPlants/PoisonousPlants.cs
@@ -10,28 +10,34 @@ class PoisonousPlants
     {
         var flowers = int.Parse(Console.ReadLine());
         var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-        var flowerField1 = new Queue<int>(input);
+        var flowerField = new Queue<int>(input);
 
         Console.WriteLine();
         var initialCount = 0;
         var days = 0;
+        var leftFlower = flowerField.Dequeue();
+        flowerField.Enqueue(leftFlower);
 
-        while (initialCount != flowerField1.Count)
-        {
-            initialCount = flowerField1.Count;
-            var previousFlower = flowerField1.Dequeue();
-            flowerField1.Enqueue(previousFlower);
+        while (initialCount != flowerField.Count)
+        {   
+            initialCount = flowerField.Count - 1;
 
             for (int i = 0; i < initialCount; i++)
             {
-                var currentFlower = flowerField1.Dequeue();
-                if (previousFlower < currentFlower)
+                var rightFlower = flowerField.Peek();
+
+                if (leftFlower < rightFlower)
                 {
-                    flowerField1.Enqueue(currentFlower);
+                    flowerField.Enqueue(leftFlower);
+                    leftFlower = flowerField.Dequeue();
                 }
-                previousFlower = currentFlower;
+                else
+                {
+                    leftFlower = flowerField.Dequeue();
+                }
             }
 
+            
             days++;
         }
     }
